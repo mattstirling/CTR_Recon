0a144a07608eaa423769372d654b073d042e6778
mstirling <mstirling@ScotiaFalcon>
2016-08-04 18:36:27 -0400

    updating

--- ANVIL/file_compare.py
+++ ANVIL/file_compare.py
@@ -11,32 +11,44 @@ in_CTR_file = 'out_ANVIL/' + 'out_ANVIL_Repo_CTR_preprocessed_file.csv'
 in_VAR_folder = 'C:/Users/mstirling/Desktop/Shared/RW/CTR Files/21-JUL-16/'
 in_VAR_file = 'out_ANVIL/' + 'out_ANVIL_Repo_VAR_preprocessed_file.csv'
 
-#out files
-out_file = 'out_ANVIL/' + 'out_ANVIL_Repo_diff_by_cell.csv'
-
 #load data
 df_CTR = pd.read_csv(in_CTR_folder+in_CTR_file)
 df_VAR = pd.read_csv(in_VAR_folder+in_VAR_file)
+print len(df_CTR.columns)
+print len(df_VAR.columns)
 
-#count rows + cols  
-print '\n' + 'pre-filter rows + cols'
-print 'CTR: ' + str(len(df_CTR.index)) + ', ' + str(len(df_CTR.columns))
-print 'RW: ' + str(len(df_VAR.index)) + ', ' + str(len(df_VAR.columns))
+#out files
+out_file_diff = 'out_ANVIL/' + 'out_ANVIL_Repo_diff.csv'
+out_file_inVAR_notCTR = 'out_ANVIL/' + 'out_ANVIL_Repo_inVAR_notCTR.csv'
+out_file_inCTR_notVAR = 'out_ANVIL/' + 'out_ANVIL_Repo_inCTR_notVAR.csv'
+out_file_columns_diff = 'out_ANVIL/' + 'out_ANVIL_Repo_diff_columns_diff.csv'
+
+#output missing rows
+df_diff = df_CTR[~np.in1d(df_CTR['Name'],df_VAR['Name'])]
+if len(df_diff.index)>0: df_diff.to_csv(in_CTR_folder + out_file_inCTR_notVAR)
+
+df_diff = df_VAR[~np.in1d(df_VAR['Name'],df_CTR['Name'])]
+if len(df_diff.index)>0: df_diff.to_csv(in_CTR_folder + out_file_inVAR_notCTR)
+
+#output difference in columns
+df_CTR_col = pd.DataFrame({'in_CTR':['Y' for col in df_CTR.columns]},index=df_CTR.columns) 
+df_VAR_col = pd.DataFrame({'in_VAR':['Y' for col in df_VAR.columns]},index=df_VAR.columns)
+df_merge_col = pd.concat([df_CTR_col,df_VAR_col],axis=1)
+df_merge_col.sort_index(axis=1,inplace=True)
+df_merge_col.to_csv(in_CTR_folder + out_file_columns_diff)
+
+#drop any rows that are not in common
+df_CTR = df_CTR[np.in1d(df_CTR['Name'],df_VAR['Name'])]
+df_VAR = df_VAR[np.in1d(df_VAR['Name'],df_CTR['Name'])]
+print len(df_CTR.columns)
+print len(df_VAR.columns)
 
 #drop any columns that are not in common
 common_cols = [col for col in df_CTR.columns if col in df_VAR.columns]
 df_CTR.drop(labels=[col for col in df_CTR.columns if col not in common_cols],axis=1,inplace=True)
 df_VAR.drop(labels=[col for col in df_VAR.columns if col not in common_cols],axis=1,inplace=True)
-
-#drop any rows that are not in common
-common_names = np.intersect1d(df_VAR.Name, df_CTR.Name)
-df_CTR = df_CTR[df_CTR.Name.isin(common_names)]
-df_VAR = df_VAR[df_VAR.Name.isin(common_names)]
-
-#count rows + cols  
-print '\n' + 'post-filter rows + cols'
-print 'CTR: ' + str(len(df_CTR.index)) + ', ' + str(len(df_CTR.columns))
-print 'RW: ' + str(len(df_VAR.index)) + ', ' + str(len(df_VAR.columns))
+print len(df_CTR.columns)
+print len(df_VAR.columns)
 
 #set index to Name
 df_CTR.set_index('Name',inplace=True)
@@ -58,14 +70,18 @@ changed_index = [df_CTR.index[i] for i in difference_locations[0]]
 changed_col = [df_CTR.columns[i] for i in difference_locations[1]]
 df_diff = pd.DataFrame({'name':changed_index,'column':changed_col,'val_CTR': changed_CTR, 'val_VAR': changed_VAR})
 
-df_diff.to_csv(in_CTR_folder + out_file)
+print 'num records match: ' + str(len(df_CTR.index))
+print 'num columns match: ' + str(len(df_CTR.columns))
+print 'num non-null cells to compare: ' + str(len(np.where(~(df_CTR.isnull() & df_VAR.isnull()))[0])) 
+print 'num cells diff: ' + str(len(df_diff.index)) 
+
+df_diff.to_csv(in_CTR_folder + out_file_diff)
 
 print len(df_diff)
 
 df_group = df_diff.groupby(['column']).size()
-#print df_group
-print len(df_group)
 
+#print only a subset of the file
 #df_diff[df_diff.column=='Asset Notional'].to_csv(in_VAR_folder + out_VAR_file)
 
 print 'done.'
--- OBJECTCHARLIE/file_compare.py
+++ OBJECTCHARLIE/file_compare.py
@@ -10,11 +10,43 @@ in_CTR_folder = 'C:/Users/mstirling/Desktop/Shared/RW/CTR Files/21-JUL-16/'
 in_CTR_file = 'out_OBJECTCHARLIE/' + 'out_OBJCHARLIE_FXFORWARD_CTR_preprocessed_file.csv'
 in_VAR_folder = in_CTR_folder
 in_VAR_file = 'out_OBJECTCHARLIE/' + 'out_OBJCHARLIE_FXFORWARD_VAR_preprocessed_file.csv'
-out_file = 'out_OBJECTCHARLIE/' + 'out_compare_diff_OBJECTCHARLIE_FXFORWARD.csv'
 
 #load data
 df_CTR = pd.read_csv(in_CTR_folder+in_CTR_file)
 df_VAR = pd.read_csv(in_VAR_folder+in_VAR_file)
+print len(df_CTR.columns)
+print len(df_VAR.columns)
+
+#out files
+out_file_diff = 'out_OBJECTCHARLIE/' + 'out_OBJECTCHARLIE_FXFORWARD_diff.csv'
+out_file_inVAR_notCTR = 'out_OBJECTCHARLIE/' + 'out_OBJECTCHARLIE_FXFORWARD_inVAR_notCTR.csv'
+out_file_inCTR_notVAR = 'out_OBJECTCHARLIE/' + 'out_OBJECTCHARLIE_FXFORWARD_inCTR_notVAR.csv'
+out_file_columns_diff = 'out_OBJECTCHARLIE/' + 'out_OBJECTCHARLIE_FXFORWARD_columns_diff.csv'
+
+#output missing rows
+df_diff = df_CTR[~np.in1d(df_CTR['Name'],df_VAR['Name'])]
+if len(df_diff.index)>0: df_diff.to_csv(in_CTR_folder + out_file_inCTR_notVAR)
+
+df_diff = df_VAR[~np.in1d(df_VAR['Name'],df_CTR['Name'])]
+if len(df_diff.index)>0: df_diff.to_csv(in_CTR_folder + out_file_inVAR_notCTR)
+
+#output difference in columns
+df_CTR_col = pd.DataFrame({'in_CTR':['Y' for col in df_CTR.columns]},index=df_CTR.columns) 
+df_VAR_col = pd.DataFrame({'in_VAR':['Y' for col in df_VAR.columns]},index=df_VAR.columns)
+df_merge_col = pd.concat([df_CTR_col,df_VAR_col],axis=1)
+df_merge_col.sort_index(axis=1,inplace=True)
+df_merge_col.to_csv(in_CTR_folder + out_file_columns_diff)
+
+#drop any rows that are not in common
+df_CTR = df_CTR[np.in1d(df_CTR['Name'],df_VAR['Name'])]
+df_VAR = df_VAR[np.in1d(df_VAR['Name'],df_CTR['Name'])]
+print len(df_CTR.columns)
+print len(df_VAR.columns)
+
+#drop any columns that are not in common
+common_cols = [col for col in df_CTR.columns if col in df_VAR.columns]
+df_CTR.drop(labels=[col for col in df_CTR.columns if col not in common_cols],axis=1,inplace=True)
+df_VAR.drop(labels=[col for col in df_VAR.columns if col not in common_cols],axis=1,inplace=True)
 print len(df_CTR.columns)
 print len(df_VAR.columns)
 
@@ -25,13 +57,6 @@ df_VAR.set_index('Name',inplace=True)
 #sort by index
 df_CTR.sort_index(axis=0,inplace=True)
 df_VAR.sort_index(axis=0,inplace=True)
-
-#drop any columns that are not in common
-common_cols = [col for col in df_CTR.columns if col in df_VAR.columns]
-df_CTR.drop(labels=[col for col in df_CTR.columns if col not in common_cols],axis=1,inplace=True)
-df_VAR.drop(labels=[col for col in df_VAR.columns if col not in common_cols],axis=1,inplace=True)
-print len(df_CTR.columns)
-print len(df_VAR.columns)
 
 #reorder columns
 df_CTR.sort_index(axis=1,inplace=True)
@@ -47,10 +72,10 @@ df_diff = pd.DataFrame({'name':changed_index,'column':changed_col,'val_CTR': cha
 
 print 'num records match: ' + str(len(df_CTR.index))
 print 'num columns match: ' + str(len(df_CTR.columns))
-print 'num non-null cells to compare: ' + str(len(np.where((df_CTR.isnull() & df_VAR.isnull()))[0])) 
+print 'num non-null cells to compare: ' + str(len(np.where(~(df_CTR.isnull() & df_VAR.isnull()))[0])) 
 print 'num cells diff: ' + str(len(df_diff.index)) 
 
-df_diff.to_csv(in_CTR_folder + out_file)
+df_diff.to_csv(in_CTR_folder + out_file_diff)
 
 print len(df_diff)
 
