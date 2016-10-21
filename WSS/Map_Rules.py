edc42bf30df66817c285f542f7e39fa5aad952c8
mstirling <mstirling@ScotiaFalcon>
2016-10-21 11:20:24 -0400

    WSS done.

---
+++ WSS/Map_Rules.py
@@ -0,0 +1,103 @@
+'''
+Created on Feb 24, 2016
+
+@author: mstirling
+'''
+import numpy as np
+import re
+
+def apply_map_rule(value,rulename):
+    
+    try:
+        if rulename == 'KO_dot_right6': return KO_dot_right6(value)
+        elif rulename == 'colon_KO_dot_right6': return colon_KO_dot_right6(value)
+        elif rulename == 'add_bracket_add_2decimals': return add_bracket_add_2decimals(value)
+        elif rulename == 'add_2decimals': return add_2decimals(value)
+        elif rulename == 'Y_to_true':return Y_to_true(value)
+        elif rulename == 'round_to_2decimal':return round_to_2decimal(value)
+        elif rulename == 'replace_bar_with_comma':return replace_bar_with_comma(value) 
+        elif rulename == 'asset_reset_freq_remap':return asset_reset_freq_remap(value)
+        elif rulename == 'comma_list_of_num_add_10decimals':return comma_list_of_num_add_10decimals(value)
+        elif rulename == 'add_colon':return add_colon(value)
+        elif rulename == 'right7':return right7(value)
+        elif rulename == 'WSS_VAR_right19_if_rec':return WSS_VAR_right19_if_rec(value)
+        elif rulename == 'WSS_remove_CTR_prefix_and_add_Rec':return WSS_remove_CTR_prefix_and_add_Rec(value)
+        else: return 'rule not in if-else tree'
+    except:
+        return value
+
+def WSS_remove_CTR_prefix_and_add_Rec(value):
+    match = re.search(r'.*[.]([0-9]*)',value)
+    if match: return match.group(1) + ' Rec'
+    else: return value
+
+def WSS_VAR_right19_if_rec(value):
+    match = re.search(r'.*([0-9]{13}) [0-9]{15}( Rec)',value)
+    if match: return match.group(1) + match.group(2)
+    else: return value
+    
+
+def right7(value):
+    return str(value)[-7:]
+
+def round_to_2decimal(value):
+    return np.round(value,0)
+
+def KO_dot_right6(value):
+    return 'KO' + value[-6:]
+
+def colon_KO_dot_right6(value):
+    return ':KO' + value[-6:]
+
+def add_bracket_add_2decimals(value):
+    date_num_USD = value.split(' ')
+    return '(' + date_num_USD[0] + ' ' '{0:.2f}'.format(float(date_num_USD[1])) + ' ' +date_num_USD[2] + ')' 
+
+def add_2decimals(value):
+    num_USD = value.split(' ')
+    return '{0:.2f}'.format(float(num_USD[0])) + ' ' +num_USD[1]
+
+def Y_to_true(value):
+    if value=='Y': return 'True'
+    else: return value
+
+def replace_bar_with_comma(value):
+    #actually replacing with ', ', not ','
+    return str(value).replace('|',', ')
+
+def asset_reset_freq_remap(value):
+    if str(value) in ('nan','NaT'):
+        return value
+        
+    dict_case = {
+                  'Mon': 'Month'
+                }
+    return dict_case.get(value,value)
+
+def add_10decimals(value):
+    return str('{0:.10f}'.format(float(value)))
+
+def comma_list_of_num_add_10decimals(value):
+    #
+    #hmmm.... this function doesn't work correct right now.
+    #
+    if str(value) in ('nan','NaT'):
+        return value
+    
+    if isinstance(value, np.ndarray):
+        preprocess_str =  ', '.join([str(i) for i in value.tolist()])
+    
+    elif isinstance(value, basestring):
+        preprocess_str =  str(value)
+    
+    else:
+        print type(value)
+        return 'missing case in mapping process'
+    
+    comma_list_val = preprocess_str.split(', ')
+    #print comma_list_val
+    return ', '.join([add_10decimals(i) for i in comma_list_val])
+    
+def add_colon(value):
+    return ':' + str(value)
+
---
+++ WSS/VAR_preprocess.py
@@ -0,0 +1,72 @@
+'''
+Created on Feb 23, 2016
+
+@author: mstirling
+'''
+import pandas as pd, ConfigParser
+from Map_Rules import apply_map_rule  # @UnresolvedImport
+
+#open config file
+config = ConfigParser.ConfigParser()
+config.read('config.ini')
+
+#in VAR files
+in_folder = config.get('filename','in_folder_VAR')
+in_file = config.get('filename','in_file_VAR')
+
+#in CTR files
+map_folder = config.get('filename','in_folder_map')
+map_file = config.get('filename','in_file_map')
+
+#out folder
+out_folder = config.get('filename','out_folder')
+out_file = config.get('filename','out_file_VAR')
+
+#open in_files
+in_df = pd.read_csv(in_folder+in_file)
+#print len(df_TRS.index)
+
+df_merge = in_df[(in_df.Filename.str.contains('fx__Sybase_WSS_FX'))]
+
+df_merge.reset_index(inplace=True,drop=True)
+print len(df_merge.index)
+
+#reorder columns
+df_merge.sort_index(axis=1,inplace=True)
+
+#open mapping rules
+df_map = pd.read_csv(map_folder+map_file)
+
+#apply transformation for alias
+#copy alias values to real column and drop alias column
+for i in df_map[~df_map['RW Alias'].isnull()].index:
+    #assume only 1 alias per column for now
+    this_col = str(df_map.at[i,'Column Name'])
+    this_alias = str(df_map.at[i,'RW Alias'])
+    
+    #copy alias values to 'this_col'
+    for j in df_merge[~df_merge[this_alias].isnull()].index:
+        df_merge.at[j,this_col] = df_merge.at[j,this_alias] 
+    
+    #drop alias column
+    df_merge.drop([this_alias],axis=1,inplace=True)
+
+#now apply 'within-cell' mapping rules
+for i in df_map['Column Name'].index:
+    this_col = str(df_map.at[i,'Column Name'])
+    this_map_rule = str(df_map.at[i,'RW Map Rule'])
+    
+    #apply the mapping rule if rule 'not nan/blank'
+    if not this_map_rule == 'nan':
+        for j in df_merge.index:
+            df_merge.at[j, this_col] = apply_map_rule(df_merge.at[j, this_col], this_map_rule) 
+
+#sort by Name
+df_merge.sort('Name', inplace = True)
+
+#write out_files
+df_merge.to_csv(out_folder + out_file,index=False)
+
+print 'done.'
+print 'from ' + in_folder
+print 'to ' + out_folder
